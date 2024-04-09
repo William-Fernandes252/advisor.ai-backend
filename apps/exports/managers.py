17a4031dcb9fe1fc9048c96b40fe18fee1d559af
William Fernandes Dias <william.winchester1967@gmail.com>
2024-04-08 21:49:42 -0300

    chore: add type annotation the manager methods

--- apps/exports/managers.py
+++ apps/exports/managers.py
@@ -1,9 +1,14 @@
+from typing import TYPE_CHECKING
+
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
+if TYPE_CHECKING:
+    from apps.exports.models import Export
+
 
 class ExportManager(models.Manager):
-    def get_latest_for_content_type(self, content_type: ContentType):
+    def get_latest_for_content_type(self, content_type: ContentType) -> "Export":
         """Return the latest export for a given content type.
 
         Args:
@@ -12,4 +17,4 @@ class ExportManager(models.Manager):
         Returns:
             Export: The corresponding export.
         """
-        return self.filter(content_type=content_type, latest=True).first()
+        return self.filter(content_type=content_type, latest=True).first()  # type: ignore[return-value]
