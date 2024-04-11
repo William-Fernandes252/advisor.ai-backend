8c82e1651114d6c7c870d22c5aaf63f4df07114c
William Fernandes Dias <william.winchester1967@gmail.com>
2024-04-11 18:40:47 -0300

    chore: add help text for the filter set fields and specify the available fields in the meta options

--- apps/papers/filters.py
+++ apps/papers/filters.py
@@ -6,25 +6,40 @@ from apps.papers import models, querysets
 class PaperFilter(filters.FilterSet):
     """Filter for the Paper model."""
 
-    title = filters.CharFilter(lookup_expr="icontains")
+    title = filters.CharFilter(
+        lookup_expr="icontains", label="Title", help_text="Filter by title."
+    )
     authors = filters.ModelMultipleChoiceFilter(
         field_name="authors__name",
         to_field_name="name",
         queryset=models.Author.objects.all(),
+        label="Authors",
+        help_text="Filter by a list of authors.",
     )
-    country = filters.CharFilter(field_name="location__country")
+    country = filters.CharFilter(
+        field_name="location__country", label="Country", help_text="Filter by country."
+    )
     keywords = filters.ModelMultipleChoiceFilter(
         field_name="keywords__name",
         to_field_name="name",
         queryset=models.Keyword.objects.all(),
         conjoined=True,
+        help_text="Filter by a set of keywords."
+        " Only papers that have all the keywords will be returned.",
     )
-    published = filters.DateTimeFromToRangeFilter()
-    search = filters.CharFilter(method="search_for_papers", label="Search")
+    published = filters.DateTimeFromToRangeFilter(
+        label="Publishing date range",
+        help_text="Filter papers published on the specified period.",
+    )
+    search = filters.CharFilter(
+        method="search_for_papers",
+        label="Search",
+        help_text="Search by the papers content.",
+    )
 
     class Meta:
         model = models.Paper
-        fields = ["title", "authors", "location", "keywords", "search"]
+        fields = ["title", "authors", "keywords", "search", "country", "published"]
 
     def search_for_papers(
         self, queryset: querysets.PaperQuerySet, name: str, value: str
