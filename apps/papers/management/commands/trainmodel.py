7f484e3705daaf7a194c1c2e970073438d2d49ac
William Fernandes Dias <william.winchester1967@gmail.com>
2024-04-10 21:43:37 -0300

    feat: implement management commands to export papers reviews data, train new models and generate suggestions

---
+++ apps/papers/management/commands/trainmodel.py
@@ -0,0 +1,39 @@
+import json
+
+from django.core.management.base import BaseCommand, CommandParser
+
+
+class Command(BaseCommand):
+    help = "Train a surprise model using the latest exported datasets."
+
+    def add_arguments(self, parser: CommandParser) -> None:
+        parser.add_argument(
+            "model",
+            nargs="?",
+            default="svd",
+            type=str,
+            help="The model type to train.",
+        )
+        parser.add_argument(
+            "--params",
+            default=None,
+            type=str,
+            help="The training parameters."
+            " If not provided, the default parameters will be used.",
+        )
+
+    def handle(self, *args, **kwargs):
+        from apps.papers.tasks import train_and_export_new_model
+
+        model_type = kwargs["model"]
+        try:
+            params = json.loads(kwargs["params"]) if kwargs["params"] else None
+        except json.JSONDecodeError:
+            self.stdout.write(
+                self.style.ERROR("Invalid JSON format for the parameters.")
+            )
+            return
+
+        train_and_export_new_model(model_type, params)
+
+        self.stdout.write(self.style.SUCCESS("Successfully trained the model."))
